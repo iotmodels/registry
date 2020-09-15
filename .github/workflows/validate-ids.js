4471989eca95b63b95a3c0f7dc390ed395f632c7
ridomin <ridomin@live.com>
2020-09-14 23:55:21 -0700

    fix indexOf

--- .github/workflows/validate-ids.js
+++ .github/workflows/validate-ids.js
@@ -5,7 +5,7 @@ import { checkIds } from '../../repo-convention.js'
 for (let i = 2; i < argv.length; i++) {
   const file = argv[i]
   console.log('\nchecking: ' + file)
-  if (file.indexOf('dtmi')>0){
+  if (file.indexOf('dtmi')>=0){
     if (fs.existsSync(file)) {
       checkIds(JSON.parse(fs.readFileSync(file, 'utf-8')))
     } else {
