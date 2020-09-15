68caacc27833a4e9ad823637952ffa7651ae5e9a
ridomin <ridomin@live.com>
2020-09-14 23:53:11 -0700

    more logs

--- .github/workflows/validate-ids.js
+++ .github/workflows/validate-ids.js
@@ -4,8 +4,8 @@ import { checkIds } from '../../repo-convention.js'
 
 for (let i = 2; i < argv.length; i++) {
   const file = argv[i]
+  console.log('\nchecking: ' + file)
   if (file.indexOf('dtmi')>0){
-    console.log('\nchecking: ' + file)
     if (fs.existsSync(file)) {
       checkIds(JSON.parse(fs.readFileSync(file, 'utf-8')))
     } else {
