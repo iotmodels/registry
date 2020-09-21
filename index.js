13882c5e5c4bda3bae4bcbaaa819c26c9ae3ca93
ridomin <ridomin@live.com>
2020-09-21 01:07:44 -0700

    render resolverd 1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { expand } from './expand-dependencies.js'
+import { dtmi2path } from './dtmi2path.js'
 
 (async () => {
   /**
@@ -23,16 +23,63 @@ import { expand } from './expand-dependencies.js'
 
   const init = () => {
     const button = gbid('search')
-    const button2 = gbid('search2')
-    const query = gbid('q')
     button.onclick = async () => {
-      bindTemplate('models-list-template', '', 'rendered')
-      const model = await expand(query.value)
-      bindTemplate('models-list-template', JSON.stringify(model, null, 2), 'rendered')
-    }
-    button2.onclick = async () => {
-      const modelResolver = 'https://model-resolver.azurewebsites.net/api/expand?id='
-      bindTemplate('models-iframe-template', `${modelResolver}${query.value}`, 'rendered')
+      const dtmi = gbid('q').value
+      bindTemplate('model-template', '', 'rendered')
+      const { modelFolder, fileName } = dtmi2path(dtmi)
+      const url = `${modelFolder}/${fileName.replace('.json', '.deps.json')}`
+      const docs = await (await window.fetch(url)).json()
+      const rootDoc = docs.filter(doc => doc['@id'] === dtmi)[0]
+
+      const addComp2Model = (name, comp) => {
+        const compPos = model.Components.push({ properties: [], telemetry: [], commands: [] })
+        const compItem = model.Components[compPos - 1]
+        compItem.name = name
+        if (Array.isArray(comp.contents)) {
+          comp.contents.forEach(c => {
+            switch (c['@type']) {
+              case 'Telemetry':
+                compItem.telemetry.push(c)
+                break
+              case 'Property':
+                compItem.properties.push(c)
+                break
+              case 'Command':
+                compItem.commands.push(c)
+                break
+            }
+          })
+        }
+      }
+
+      const model = {}
+      model.id = rootDoc['@id']
+      model.displayName = rootDoc.displayName
+      model.Default = { properties: [], telemetry: [], commands: [] }
+      model.Components = []
+      if (Array.isArray(rootDoc.contents)) {
+        rootDoc.contents.forEach(c => {
+          switch (c['@type']) {
+            case 'Telemetry':
+              model.Default.telemetry.push(c)
+              break
+            case 'Property':
+              model.Default.properties.push(c)
+              break
+            case 'Command':
+              model.Default.commands.push(c)
+              break
+            case 'Component':
+              if (typeof c.schema !== 'object') {
+                const iface = docs.filter(doc => doc['@id'] === c.schema)[0]
+                addComp2Model(c.name, iface)
+              }
+              break
+          }
+        })
+      }
+      console.log(model)
+      bindTemplate('model-template', model, 'rendered')
     }
   }
   init()
